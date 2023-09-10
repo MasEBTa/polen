e099a18ba008f8d7fadb7ae3285522669e189fe4
MasEBTA <ebta.sb@gmail.com>
2023-09-10 22:48:03 +0700

    [IMPROVE] -> edit return value

--- utils/common/load_env.go
+++ utils/common/load_env.go
@@ -1,15 +1,10 @@
 package common
 
 import (
-	"fmt"
-
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() error {
 	err := godotenv.Load()
-	if err != nil {
-		return fmt.Errorf("error loading .env file with error : %v", err)
-	}
 	return err
 }
