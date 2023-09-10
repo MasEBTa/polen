6c0fa5537756fe1cb9d4b4210cc7e3c36dabf0da
MasEBTA <ebta.sb@gmail.com>
2023-09-10 16:22:51 +0700

    [FEATURE] -> adding new server controller
    server file is used to control connection to database

---
+++ delivery/server.go
@@ -0,0 +1,62 @@
+package delivery
+
+import (
+	"fmt"
+	"polen/config"
+	"polen/delivery/controller/api"
+
+	"polen/delivery/middleware"
+	"polen/manager"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type Server struct {
+	ucManager manager.UseCaseManager
+	engine    *gin.Engine
+	host      string
+	log       *logrus.Logger
+}
+
+func (s *Server) Run() {
+	s.initMiddlewares()
+	s.initControllers()
+	err := s.engine.Run(s.host)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s *Server) initMiddlewares() {
+	s.engine.Use(middleware.LogRequestMiddleware(s.log))
+}
+
+func (s *Server) initControllers() {
+	rg := s.engine.Group("/api/v1")
+	api.NewAuthController(s.ucManager.UserUseCase(), s.ucManager.AuthUseCase(), rg).Route()
+}
+
+func NewServer() *Server {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		fmt.Println(err)
+	}
+	infraManager, err := manager.NewInfraManager(cfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	repoManager := manager.NewRepoManager(infraManager)
+	useCaseManager := manager.NewUsecaseManager(repoManager)
+
+	host := fmt.Sprintf("%s:%s", cfg.ApiHost, cfg.ApiPort)
+	log := logrus.New()
+
+	engine := gin.Default()
+	return &Server{
+		ucManager: useCaseManager,
+		engine:    engine,
+		host:      host,
+		log:       log,
+	}
+}
