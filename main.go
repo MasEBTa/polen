c1528b12b38ae9eef91539a48d7a98051af8c7f5
MasEBTA <ebta.sb@gmail.com>
2023-09-13 10:03:50 +0700

    [UPDATE]-> main

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func main() {
 	// Loop tak terbatas untuk menjalankan pembaruan database
 	for range ticker.C {
 		// Panggil fungsi untuk melakukan pembaruan database di sini
+		// usecase.UpdateDepositeDatabe()
 		// Contoh: updateDatabase()
 		fmt.Println("Melakukan pembaruan database...")
 	}
