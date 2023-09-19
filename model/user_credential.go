8e252f32d54789be945d8149aba8b4b3a105154a
MasEBTA <ebta.sb@gmail.com>
2023-09-19 13:28:00 +0700

    merge

--- model/user_credential.go
+++ model/user_credential.go
@@ -1,7 +1,6 @@
 package model
 
 type UserCredential struct {
-<<<<<<< HEAD
 	Id       string
 	Username string
 	Email    string
@@ -9,11 +8,4 @@ type UserCredential struct {
 	VANumber string
 	Role     string
 	IsActive bool
-=======
-	Id       string `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
-	IsActive bool   `json:"is_active"`
->>>>>>> dev/akbar
 }
